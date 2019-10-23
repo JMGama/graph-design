38e5a04c6aef8234f20ba15926ac02436c0304b5
Jose Manuel Gama Estrada <jgama@ia.com.mx>
2019-10-22 23:21:04 -0500

    Initialize component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Grid } from "./components/Grid";
 
 function App() {
   return (
     <div className="App">
+      <Grid></Grid>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
