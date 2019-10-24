7c7ebe6a115f30e3a47682daeb14414b1d703901
Jose Manuel Gama Estrada <jgama@ia.com.mx>
2019-10-23 23:00:19 -0500

    use node in grid

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import { Node } from '../Node'
+import './style.css'
 
 export const Grid = () => {
-  return <h2>This is a Grid</h2>;
+  const grid = initGrid()
+  return (
+    <div className="grid">
+      <table>
+        <tbody>
+          {grid.map((row, rowIndx) => {
+            return (
+              <tr key={rowIndx} id={`row-${rowIndx}`}>
+                {row.map((col, colIndx) => {
+                  return (
+                    <Node key={colIndx} row={rowIndx} col={colIndx}></Node>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>);
 };
+
+const initGrid = () => {
+  const grid = [];
+  for (let row = 0; row < 25; row++) {
+    const current_row = []
+    for (let col = 0; col < 25; col++) {
+      const current_col = [];
+      current_row.push(current_col)
+    }
+    grid.push(current_row)
+  }
+  return grid
+
+}
